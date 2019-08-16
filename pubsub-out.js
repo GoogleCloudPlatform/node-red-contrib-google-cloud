f739c6ad82357b8ea34b33d4d12abab9aaf5fae9
Neil Kolban <kolban@google.com>
2019-08-16 18:15:29 +0000

    #21

--- pubsub-out.js
+++ pubsub-out.js
@@ -11,7 +11,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-
+/* jshint esversion: 8 */
 module.exports = function(RED) {
     "use strict";
 
@@ -52,6 +52,7 @@ module.exports = function(RED) {
     function GoogleCloudPubSubOutNode(config) {
         let topic  = null;
         let pubsub = null;
+        const messageQueue = [];
         RED.nodes.createNode(this, config);
 
         const node = this;
@@ -74,7 +75,11 @@ module.exports = function(RED) {
             if (msg == null || !msg.payload || msg.payload == "") {
                 return;
             }
+
+            // If we don't have a topic, then this is likely because we have received a request to
+            // publish a message while we are still waiting for access to the topic from GCP.
             if (topic === null) {
+                messageQueue.push(msg);
                 return;
             }
             node.status(STATUS_PUBLISHING);
@@ -113,6 +118,14 @@ module.exports = function(RED) {
         pubsub.topic(config.topic).get().then((data) => {
             topic = data[0];
             node.status(STATUS_CONNECTED);
+
+            // We may have been asked to process messages BEFORE the topic access has been received.  In this case,
+            // the messages will have been queued.  We pull the messages from the queue and publish them one at
+            // a time.
+            while(messageQueue.length > 0) {
+                const msg = messageQueue.shift();
+                topic.publish(RED.util.ensureBuffer(msg.payload));
+            }
         }).catch((reason) => {
             node.status(STATUS_DISCONNECTED);
             node.error(reason);
