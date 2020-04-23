2d2ef8353faae1fbb18f55c037c8a3f065b27e73
James Tharpe <jimmy.tharpe@gmail.com>
2020-04-23 17:25:09 -0400

    Support input and output options

--- translate.js
+++ translate.js
@@ -14,13 +14,16 @@
  * limitations under the License.
  *
  * This node provides DLP processing.  We will assume that msg.payload
- * contains the original data that may contain sensistive data.
+ * contains the original data that may contain sensitive data.
  * 
  * The configuration for the node includes:
  *
  */
 /* jshint esversion: 8 */
-module.exports = function(RED) {
+
+//@ts-check
+
+module.exports = function (RED) {
     "use strict";
     const NODE_TYPE = "google-cloud-translate";
     const translate = require('@google-cloud/translate');
@@ -29,9 +32,12 @@ module.exports = function(RED) {
     function TranslateNode(config) {
         RED.nodes.createNode(this, config);
         const node = this;
+        const context = node.context();
         const projectId = config.projectId;
-        const sourceLanguageCode = config.sourceLanguageCode;
-        const targetLanguageCode = config.targetLanguageCode;
+        const sourceLanguageCodeType = config.sourceLanguageCodeType || "str";
+        const targetLanguageCodeType = config.targetLanguageCodeType || "str";
+        const outputFormat = config.outputFormat || "full";
+        const displayLanguageCode = Intl.DateTimeFormat().resolvedOptions().locale || "en";
 
         let credentials = null;
         if (config.account) {
@@ -42,13 +48,69 @@ module.exports = function(RED) {
         /**
          * Extract JSON service account key from "google-cloud-credentials" config node.
          */
-        
         function GetCredentials(node) {
             return JSON.parse(RED.nodes.getCredentials(node).account);
         }
 
-        // Called when a message arrives at the node.
+        /**
+         * Get supported languages from global context. If not found, load from Google Could Translate and store in global context.
+         *
+         * @returns List of supported languages for translation
+         */
+        async function getSupportedLanguages() {
+            let supportedLanguages = context.global.get("google-cloud-translate-languages");
+
+            if (!supportedLanguages) {
+                const request = {
+                    "displayLanguageCode": displayLanguageCode,
+                    "parent": translationServiceClient.locationPath(projectId, "global")
+                };
+                supportedLanguages = await translationServiceClient.getSupportedLanguages(request)[0].languages;
+                context.global.set("google-cloud-translate-languages", supportedLanguages);
+            }
+
+            return supportedLanguages;
+        }
+
+        /**
+         * Gets the language codes for the specified languages
+         * 
+         * @param {string[]} languages
+         */
+        async function getLanguageCodes(...languages) {
+            let supportedLanguages = await getSupportedLanguages();
+            return languages.map(language =>
+                language &&
+                supportedLanguages
+                    .find(availableLanguage =>
+                        availableLanguage.languageCode === language
+                        || availableLanguage.displayName === language
+                        || availableLanguage.displayName.startsWith(language)
+                    )
+                || { languageCode: language }
+            ).map(l => l.languageCode);
+        }
+
+        /**
+         * Called when a message arrives at the node.
+         * 
+         * @param {{ payload: any; }} msg
+         */
         async function Input(msg) {
+            const [sourceLanguageCode, targetLanguageCode] = await getLanguageCodes(
+                RED.util.evaluateNodeProperty(
+                    config.sourceLanguageCode,
+                    sourceLanguageCodeType,
+                    node,
+                    msg
+                ) || "",
+                RED.util.evaluateNodeProperty(
+                    config.targetLanguageCode,
+                    targetLanguageCodeType,
+                    node,
+                    msg
+                ),
+            );
             const request = {
                 "contents": [msg.payload],
                 "sourceLanguageCode": sourceLanguageCode,
@@ -57,12 +119,14 @@ module.exports = function(RED) {
                 "parent": translationServiceClient.locationPath(projectId, "global")
             };
             try {
-                const responseArray = await translationServiceClient.translateText(request);
-                msg.payload = responseArray[0];
+                const [response] = await translationServiceClient.translateText(request);
+                msg.payload = outputFormat === "full"
+                    ? response
+                    : response.translations[0].translatedText;
                 node.send(msg);
             }
-            catch(e) {
-                console.log(e);
+            catch (e) {
+                node.error(e, msg);
             }
         } // Input
 
