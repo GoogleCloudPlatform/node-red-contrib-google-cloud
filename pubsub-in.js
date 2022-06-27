67c44c4e9a7866f4295812fff134922712a448fe
Chris Neave <cneave@gmail.com>
2022-06-28 07:51:57 +1000

    set git email

--- pubsub-in.js
+++ pubsub-in.js
@@ -81,7 +81,7 @@ module.exports = function(RED) {
                     'attributes': message.attributes,
                     'id': message.id,
                     'orderingKey': message.orderingKey,
-                    'publishTime': message.publishTime,
+                    'publishTime': message.publishTime, //modified publishedTime to publishTime
                     'received': message.received
                 }
             };
