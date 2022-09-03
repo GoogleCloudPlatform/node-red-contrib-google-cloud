bffa9726bdfb11f53a611ff3504cc489e94c087d
Yu-hua Yang <yuhuayang@google.com>
2022-09-02 19:34:24 -0700

    Fix: Added Project ID config for PubSub

--- pubsub-in.js
+++ pubsub-in.js
@@ -56,6 +56,11 @@ module.exports = function(RED) {
 
         const node = this;
 
+        let projectId = config.projectId;
+        if (!projectId || projectId.trim().length == 0) {
+            projectId = null;
+        }
+
         let options = {};
 
         if (!config.subscription) {
@@ -118,14 +123,18 @@ module.exports = function(RED) {
         // is an error.  If both are supplied, then credentials will be used.
         if (credentials) {
             pubsub = new PubSub({
+                "projectId": projectId,
                 "credentials": credentials
             });
         } else if (keyFilename) {
             pubsub = new PubSub({
+                "projectId": projectId,
                 "keyFilename": keyFilename
             });
         } else {
-            pubsub = new PubSub({});
+            pubsub = new PubSub({
+                 "projectId": projectId,
+            });
         }
 
         node.status(STATUS_CONNECTING);                              // Flag the node as connecting.
--- pubsub-out.js
+++ pubsub-out.js
@@ -58,6 +58,11 @@ module.exports = function(RED) {
 
         const node = this;
 
+        let projectId = config.projectId;
+        if (!projectId || projectId.trim().length == 0) {
+            projectId = null;
+        }
+
         let credentials = null;
         if (config.account) {
             credentials = GetCredentials(config.account);
@@ -99,14 +104,18 @@ module.exports = function(RED) {
         // is an error.  If both are supplied, then credentials will be used.
         if (credentials) {
             pubsub = new PubSub({
+                "projectId": projectId,
                 "credentials": credentials
             });
         } else if (keyFilename) {
             pubsub = new PubSub({
+                "projectId": projectId,
                 "keyFilename": keyFilename
             });
         } else {
-            pubsub = new PubSub({});
+            pubsub = new PubSub({
+                "projectId": projectId,
+            });
         }
 
         node.status(STATUS_CONNECTING);
